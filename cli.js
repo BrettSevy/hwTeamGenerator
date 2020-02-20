bc9e3470e0b05dbfbbe9b9db4549643466017c3b
Brett Sevy <justbrettt@gmail.com>
2020-02-19 20:34:22 -0600

    prompts working and switching between different job, card and html and cards generating

--- cli.js
+++ cli.js
@@ -1,14 +1,34 @@
-const promptForInfo = require("./lib/app")
+const createManager = require("./lib/app")
 
 
 
-async function startProgram() {
-    let { name, id, title, email} = await promptForInfo()
-    console.log(name, id, title, email);
-  }
+// createManager()
+    
 
 
 
+
+  // {
+  //   type: "input",
+  //   name: "name",
+  //   message: "What is the Manager's name?"
+  // },
+  // {
+  //   type: "input",
+  //   name: "id",
+  //   message: "What is the manager's ID number?"
+  // },
+  // {
+  //   type: "input",
+  //   name: "email",
+  //   message: "What is the manager's email address?"
+  // },
+  // {
+  //   type: "input,
+  //   name: "title",
+  //   message: "What is the employee's job title?",
+  //   choices: ["Manager", "Engineer", "Intern"]
+  // }
 
 
 
